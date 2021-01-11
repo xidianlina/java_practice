d0a5521b93d2b91866820811d7b17cf4dc7db33f
李娜 <lina.2017@bytedance.com>
2021-01-11 21:28:38 +0800

    越过泛型检查

---
+++ src/com/practice/reflect/Demo.java
@@ -0,0 +1,30 @@
+package com.practice.reflect;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+/*
+ * 通过反射越过泛型检查
+ *
+ * 例如：有一个String泛型的集合，怎样能向这个集合中添加一个Integer类型的值？
+ */
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("aaa");
+        strList.add("bbb");
+
+        //获取ArrayList的Class对象，反向的调用add()方法，添加数据
+        //得到 strList 对象的字节码对象
+        Class listClass = strList.getClass();
+        //获取add()方法
+        Method m = listClass.getMethod("add", Object.class);
+        //调用add()方法
+        m.invoke(strList, 100);
+
+        //遍历集合
+        for (Object obj : strList) {
+            System.out.println(obj);
+        }
+    }
+}
